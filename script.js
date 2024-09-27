f03656da573d719380d3fe59cc100da0175da434
leo-peres <leoviep@gmail.com>
2024-09-27 10:29:32 -0300

    code improvements

--- script.js
+++ script.js
@@ -24,35 +24,135 @@ function cellFactory(row, col) {
 
 }
 
+function winningCellsFactory(cells) {
+
+    let xWable = true;
+    let oWable = true;
+
+    let xW = false;
+    let oW = false;
+
+    const xWinnable = () => xWable;
+    const oWinnable = () => oWable;
+
+    const xWin = () => xW;
+    const oWin = () => oW;
+
+    const nEmpty = () => cells.filter((c) => !c.isMarked()).length;    
+    //const isEmpty = () => nEmpty(cells) == 3
+
+    const hasOnlyMark = (mark) => !cells.some((c) => c.isMarked() && c.getMark() !== mark);
+
+    const winnable = (mark, rem) => hasOnlyMark(mark) && rem >= nEmpty(cells);
+    const win = (mark) => hasOnlyMark(mark) && nEmpty() == 0;
+
+    const update = (xRem, oRem) => {
+
+        console.log(`${xRem} ${oRem}`)
+
+        if(xWable)
+            xWable = winnable("X", xRem);
+        if(oWable)
+            oWable = winnable("O", oRem);
+
+        xW = win("X");
+        oW = win("O");
+
+    }
+
+    const reset = () => {
+
+        xWable = true;
+        oWable = true;
+
+        xW = false;
+        oW = false;
+
+    }
+
+    return {
+
+        xWinnable,
+        oWinnable,
+        xWin,
+        oWin,
+        update,
+        reset
+
+    };
+
+}
+
 const gameBoard = (() => {
 
+    let dim = 3;
+
+    let xRemaining = 5;
+    let oRemaining = 4;
+
     const gameBoardArr = [];
-    for(let i = 0; i < 3; i++) {
+    for(let i = 0; i < dim; i++) {
         gameBoardArr.push([]);
-        for(let j = 0; j < 3; j++)
+        for(let j = 0; j < dim; j++)
             gameBoardArr[i].push(cellFactory(i + 1, j + 1));
     }
 
     const getCell = (row, col) => gameBoardArr[row-1][col-1];
 
-    const nEmptyCells = () => {
+    const winCells = [];
 
-        let row1c = gameBoardArr[0].filter((cell) => !cell.isMarked()).length;
-        let row2c = gameBoardArr[1].filter((cell) => !cell.isMarked()).length;
-        let row3c = gameBoardArr[2].filter((cell) => !cell.isMarked()).length;
+    const buildWinCells = () => {
 
-        return row1c + row2c + row3c;
+        winCells.length = 0;
 
-    }
+        for(let i = 1; i <= dim; i++) {
+            let row = [];
+            for(let j = 1; j <= dim; j++)
+                row.push(getCell(i, j));
+            winCells.push(winningCellsFactory(row));
+        }
 
-    const setMark = (row, col, mark) => {gameBoardArr[row-1][col-1].setMark(mark)}
+        for(let j = 1; j <= dim; j++) {
+            let col = [];
+            for(let i = 1; i <= dim; i++)
+                col.push(getCell(i, j));
+            winCells.push(winningCellsFactory(col));
+        }
+
+        const mainDiag = [];
+        for(let k = 1; k <= dim; k++)
+            mainDiag.push(getCell(k, k));
+        winCells.push(winningCellsFactory(mainDiag));
+
+        const offDiag = [];
+        for(let k = 1; k <= dim; k++)
+            offDiag.push(getCell(k, dim - k + 1));
+        winCells.push(winningCellsFactory(offDiag));
+
+    };
+
+    buildWinCells();
+
+    const nEmptyCells = () => xRemaining + oRemaining;
+
+    const setMark = (row, col, mark) => {
+        gameBoardArr[row-1][col-1].setMark(mark)
+        if(mark == "X")
+            xRemaining--;
+        else
+            oRemaining--;
+    };
+
     const getMark = (row, col) => gameBoardArr[row-1][col-1].getMark();
 
-    const clear = () => {
+    const reset = (xFirst) => {
+        xRemaining = 4 + (xFirst ? 1 : 0);
+        oRemaining = 4 + (xFirst ? 0 : 1);
         gameBoardArr.forEach((row) => {row.forEach((cell) => {cell.clearMark();});});
-    }
+        winCells.forEach((wcells) => wcells.reset());
+    };
 
-    const printBoard = () => {
+    const print = () => {
 
         let row1 = getMark(1, 1) + getMark(1, 2) + getMark(1, 3) + "\n";
         let row2 = getMark(2, 1) + getMark(2, 2) + getMark(2, 3) + "\n";
@@ -62,68 +162,18 @@ const gameBoard = (() => {
 
     };
 
-    const winCells = [
-        [getCell(1, 1), getCell(1, 2), getCell(1, 3)],
-        [getCell(2, 1), getCell(2, 2), getCell(2, 3)],
-        [getCell(3, 1), getCell(3, 2), getCell(3, 3)],
-        [getCell(1, 1), getCell(2, 1), getCell(3, 1)],
-        [getCell(1, 2), getCell(2, 2), getCell(3, 2)],
-        [getCell(1, 3), getCell(2, 3), getCell(3, 3)],
-        [getCell(1, 1), getCell(2, 2), getCell(3, 3)],
-        [getCell(3, 1), getCell(2, 2), getCell(1, 3)]
-    ];
+    const checkWin = () => winCells.some((wcells) => wcells.xWin() || wcells.oWin());
+    const checkTie = () => !winCells.some((wcells) => wcells.xWinnable() || wcells.oWinnable());
 
-    const checkWin = () => {
+    const checkResult = () => {
 
-        let hasAWinner = false;
+        winCells.forEach((wcells) => wcells.update(xRemaining, oRemaining));
+        if(checkWin())
+            return "w";
+        else if(checkTie())
+            return "t";
 
-        for(let i = 0; i < 8; i++) {
-            let wcells = winCells[i];
-            firstMark = wcells[0].getMark();
-            if(wcells.every((cell) => cell.isMarked() && cell.getMark() === firstMark)) {
-                hasAWinner = true;
-                break;
-            }         
-        }
-
-        return hasAWinner;
-
-    };
-
-    const checkTie = (mark, rem, omark, orem) => {
-
-        let emptyCells = nEmptyCells();
-
-        const nEmpty = (wcells) => {
-            return wcells.filter((cell) => !cell.isMarked()).length;
-        }
-
-        const isEmpty = (wcells) => {
-            return nEmpty(wcells) == 3;
-        }
-
-        const hasOnlyMark = (wcells, mark) => {
-
-            if(!isEmpty(wcells)) {
-                if(wcells.some((cell) => cell.isMarked() && cell.getMark() !== mark))
-                    return false;
-                else
-                    return true;
-            }
-
-            return true;           
-
-        }
-
-        const isWinnable = (wcells, mark, rem, orem) => {
-            let empty = nEmpty(wcells);
-            return hasOnlyMark(wcells, mark) &&
-                   empty <= rem &&
-                   emptyCells - empty >= orem;
-        }
-
-        return winCells.filter((wcells) => isWinnable(wcells, mark, rem, orem)).length == 0 &&
-               winCells.filter((wcells) => isWinnable(wcells, omark, orem, rem)).length == 0;
+        return ""
 
     };
 
@@ -131,10 +181,9 @@ const gameBoard = (() => {
 
         setMark,
         getMark,
-        checkWin,
-        checkTie,
-        clear,
-        printBoard
+        checkResult,
+        reset,
+        print
 
     };
 
@@ -206,27 +255,28 @@ const gameControl = (() => {
         player1RemainingMoves = 5;
         player2RemainingMoves = 4;
 
-        gameBoard.clear();
+        gameBoard.reset(true);
 
         while(n_rounds < 9) {
 
-            gameBoard.printBoard();
+            gameBoard.print();
             playRound();
             
             n_rounds++;
 
-            if(gameBoard.checkWin()) {
-                console.log(`${getOtherPlayerName()} WINS!`)
+            let result = gameBoard.checkResult();
+            if(result === "w") {
+                console.log(`${getOtherPlayerName()} WINS!`);
                 break;
             }
-            if(gameBoard.checkTie(getCurrentPlayerMark(), getCurrentPlayerRemainingMoves(), getOtherPlayerMark(), getOtherPlayerRemainingMoves())) {
+            if(result === "t") {
                 console.log("A TIE!");
                 break;
             }
 
         }
 
-        gameBoard.printBoard();
+        gameBoard.print();
 
     }
 
